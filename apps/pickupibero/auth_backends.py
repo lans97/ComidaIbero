6fd41ec430506334fc4a9588e5ade0269cc77cad
Luis Silva <luis.silva1mon@gmail.com>
2023-11-20 17:09:00 -0600

    Update auth_backends.py

--- apps/pickupibero/auth_backends.py
+++ apps/pickupibero/auth_backends.py
@@ -1,28 +1,24 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
-from .iberoApi import ibero_auth  # Import your API authentication function
+from .iberoApi import ibero_auth
 
 User = get_user_model()
 
 class IberoAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        # Check if the user is registered locally
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            # If not registered locally, ask the API for authentication
             api_user = ibero_auth(username, password)
             if api_user:
-                # If API authentication is successful, create a local user
                 user = User.objects.create_user(username=api_user['username'])
                 user.password = make_password(password)
                 user.save()
             else:
-                return None  # API authentication failed, return None
+                return None
 
-        # Check local authentication
         if user.check_password(password):
-            return user  # Local authentication successful
+            return user
         else:
-            return None  # Local authentication failed
+            return None
