72f3b8522cd7cfc250a04ba1eeb5c636e9e15f43
Luis Silva <luis.silva1mon@gmail.com>
2023-11-20 17:08:22 -0600

    Update iberoApi.py

--- apps/pickupibero/iberoApi.py
+++ apps/pickupibero/iberoApi.py
@@ -3,11 +3,3 @@ def ibero_auth(username, password):
         return { 'username': username }
     else:
         return None
-    # Your logic to authenticate with the external API
-    # Return user information if authentication is successful, else return None
-    # Example:
-    # api_response = make_api_request(username, password)
-    # if api_response and api_response.get('authenticated'):
-    #     return {'username': username, 'other_data': api_response['other_data']}
-    # else:
-    #     return None
