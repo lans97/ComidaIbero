7f0bbc43f5bfd5eb64b4ee6673d3aade081b827e
Luis Silva <luis.silva1mon@gmail.com>
2023-11-30 14:32:01 -0600

    Modificación a iberoApi

--- apps/pickupibero/iberoApi.py
+++ apps/pickupibero/iberoApi.py
@@ -1,5 +1,9 @@
+import re
+
 def ibero_auth(username, password):
-    if username == "200490-A" and password == "Contra123$":
+    user_patern = r'^\d{6}-[A-Z0-9]$'
+    password_pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10}$'
+    if re.match(user_patern, username) and re.match(password_pattern, password):
         return { 'username': username }
     else:
         return None
